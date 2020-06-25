65a98e5760f2bdb49fc04ab80f0660b91c2b6d52
albertogibin <50101656+albertogibin@users.noreply.github.com>
2020-06-25 12:12:57 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -20,7 +20,7 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 4,
+        value: 400,
         density: {
           enable: true,
           value_area: 800
